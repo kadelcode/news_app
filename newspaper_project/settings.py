74838d945d5adb3c882d0156c85088f2666d9e3d
kadelcode <kadelcode@gmail.com>
2022-09-21 15:09:28 +0100

    DEBUG value corrected

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = Config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = Config("DEBUG", cast = bool)
+DEBUG = config("DEBUG", cast=bool)
 
 ALLOWED_HOSTS = ["*"]
 
