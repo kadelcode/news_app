87610477929c6ba09192452b689059d59222030c
kadelcode <kadelcode@gmail.com>
2022-09-28 23:46:16 +0100

    Hot fix

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'newspaper_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'newspaper_project/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
