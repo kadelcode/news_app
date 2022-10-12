f8db97282de4f8faf26b7539a22d764cb29597dc
kadelcode <kadelcode@gmail.com>
2022-10-12 20:50:08 +0100

    signup form test

--- pages/tests.py
+++ pages/tests.py
@@ -39,3 +39,10 @@ class SignupPageTest(TestCase):
         response = self.client.get(reverse('signup'))
         self.assertEqual(response.status_code,200)
         self.assertTemplateUsed(response,'registration/signup.html')
+
+    # Test for the signup form
+    def test_signup_form(self):
+        new_user = get_user_model().objects.create_user(self.username, self.email)
+        self.assertEqual(get_user_model().objects.all().count(),1)
+        self.assertEqual(get_user_model().objects.all()[0].username, self.username)
+        self.assertEqual(get_user_model().objects.all()[0].email, self.email)
